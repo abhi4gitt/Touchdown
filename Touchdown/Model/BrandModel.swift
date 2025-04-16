09cefe007ed7bebec3ce3d055d969bf0445046e2
abhi4gitt <raiabhishek631@gmail.com>
2025-04-16 23:34:58 +0530

    Horizontal Grid Layout for the brands

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Abhishek on 16/04/25.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
---
+++ Touchdown/View/Home/BrandItemView.swift
@@ -0,0 +1,30 @@
+//
+//  BrandItemView.swift
+//  Touchdown
+//
+//  Created by Abhishek on 16/04/25.
+//
+
+import SwiftUI
+
+struct BrandItemView: View {
+    
+    let brand: Brand
+    
+    var body: some View {
+        Image(brand.image)
+            .resizable()
+            .scaledToFit()
+            .padding()
+            .background(Color.white.cornerRadius(12))
+            .background(
+                RoundedRectangle(cornerRadius: 12).stroke(Color.gray, lineWidth: 1)
+            )
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    BrandItemView(brand: brands[0])
+        .padding()
+        .background(colorBackground)
+}
